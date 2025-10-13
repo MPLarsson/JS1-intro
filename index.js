83b0426a2b7e4a1d437fd3b3c577fbff05856ddc
MPLarsson <Mattias.Larsson@stud.sundsgarden.se>
2025-10-13 09:36:28 +0200

    Added "Hello World"

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const name = "Mattias Larsson";
+/* const name = "Mattias Larsson";
 let age = "32";
 let color = "green";
 
 let x = 12;
-let y = 24;
+let y = 24; */
 
 /* alert("Please click F12"); */
 
@@ -11,13 +11,13 @@ console.log("Hello World!");
 
 /* console.log(name,age,color); */
 
-function showVariables() {
+/* function showVariables() {
     let name = "My name is Mattias Larsson";
     let age = 25;
     let color = "My favourite color is Green";
 
     console.log(name,age,color);
-}
+} */
 
 console.log(x+y);
 
