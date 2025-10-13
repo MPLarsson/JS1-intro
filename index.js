f795cf28bf41ea491a1e360380c3694e2432686f
MPLarsson <Mattias.Larsson@stud.sundsgarden.se>
2025-10-13 09:32:35 +0200

    Made two functions to utilize console.log

---
+++ index.js
@@ -0,0 +1,28 @@
+const name = "Mattias Larsson";
+let age = "32";
+let color = "green";
+
+let x = 12;
+let y = 24;
+
+/* alert("Please click F12"); */
+
+console.log("Hello World!");
+
+/* console.log(name,age,color); */
+
+function showVariables() {
+    let name = "My name is Mattias Larsson";
+    let age = 25;
+    let color = "My favourite color is Green";
+
+    console.log(name,age,color);
+}
+
+console.log(x+y);
+
+console.log(y-x);
+
+console.log(y/x);
+
+console.log(x*y);
