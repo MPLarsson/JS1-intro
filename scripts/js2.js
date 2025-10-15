7650a1c85365347fd627e6db5b6fa4f2d597997e
MPLarsson <Mattias.Larsson@stud.sundsgarden.se>
2025-10-15 12:52:25 +0200

    finished the assignment
    Initialized two variables that are given values by two functions.
    
    The first one takes input from the user and converts it to numbers and removes the $ symbol. Added some "if" statements incase the user decides to type something other than the intended input. The function ends by returning price.
    
    The other function takes the price variable and reduce it by 10% and returns that value to the reducedPrice variable.
    
    Added some functionality by adding these functions to buttons for the user to interact with using getElementById and addEventListener.
    
    Added the ability to see the current value of current price and current reduced price in the html just for fun.

--- scripts/js2.js
+++ scripts/js2.js
@@ -1,4 +1,4 @@
-//Use "*variablename*.toString" to convert tp string.
+//use "*variablename*.toString" to convert tp string.
 
 //use "parseInt or parseFloat" to convert to numbers.
 
@@ -11,14 +11,38 @@ if (price ==null || price =="") {
 }
  */
 
+let price = 0;
+let reducedPrice = 0;
+
+document.getElementById("currentPrice").innerHTML = price;
+document.getElementById("currentDiscount").innerHTML = reducedPrice;
+
 document.getElementById("pricePromptBtn").addEventListener("click",pricePrompt);
 
 function pricePrompt () {
-    let price = parseFloat(prompt("Enter a price:","0"));
-    let text;
-    if (price ==null || price =="") {
-        text = "No price added,";
-    } else {
-        text = `The price is $${price}`;
+    let input = prompt("Enter a price:","$");
+    
+    if (input ==null || input =="") {
+        alert("No price added")
+        return;
+    } 
+    price = parseFloat(input.replace("$",""));
+
+    if (isNaN(price)) {
+        alert("That's not a valid price.")
+        return;
     }
+
+    let text = `The price is $${price}`;
+    alert(text);
+    document.getElementById("currentPrice").innerHTML = `$${price}`;
+    return price;
+}
+
+document.getElementById("reducedPriceBtn").addEventListener("click",priceReduction);
+
+function priceReduction () {
+    reducedPrice = price * 0.9;
+    alert(`Your discount is $${reducedPrice}!`);
+    document.getElementById("currentDiscount").innerHTML = `$${reducedPrice}`;
 }
